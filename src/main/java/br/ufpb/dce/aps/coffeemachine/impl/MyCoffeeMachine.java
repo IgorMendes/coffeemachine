bad20159e693c9ac54231343bf2f67ceae6d2b34
unknown <igor.souza@dce.ufpb.br>
2014-07-15 09:59:46 -0300

    Task 1.04 finished for COMPOR

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -2,25 +2,32 @@ package br.ufpb.dce.aps.coffeemachine.impl;
 
 import net.compor.frameworks.jcf.api.ComporFacade;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
+import br.ufpb.dce.aps.coffeemachine.CoffeeMachineException;
 import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 
-public class MyCoffeeMachine extends ComporFacade implements CoffeeMachine{
-	
-	int totalcoin;
-	private  ComponentsFactory factory;
-	
-	public MyCoffeeMachine(ComponentsFactory factory){
-		
+public class MyCoffeeMachine extends ComporFacade implements CoffeeMachine {
+
+	int totalCoin;
+	private ComponentsFactory factory;
+
+	public MyCoffeeMachine(ComponentsFactory factory) {
+
 		this.factory = factory;
 		factory.getDisplay().info("Insert coins and select a drink!");
 	}
 
-	public void insertCoin(Coin coin) {
+	public void insertCoin(Coin coin) throws CoffeeMachineException {
 		
-		totalcoin += coin.getValue();
-		factory.getDisplay().info("Total: US$ " + totalcoin / 100 + "." +totalcoin % 100);
 		
+		try{
+			totalCoin += coin.getValue();
+			factory.getDisplay().info(
+					"Total: US$ " + totalCoin / 100 + "." + totalCoin % 100);
+
+		} catch(NullPointerException e){
+			throw new CoffeeMachineException("erro");
+		}
 		
 	}
 
