9b4d20a20e15b833acd602c0581ae83d847b4b04
unknown <igor.souza@dce.ufpb.br>
2014-07-20 20:36:06 -0300

    Task 1.05 finished for COMPOR

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -18,17 +18,26 @@ public class MyCoffeeMachine extends ComporFacade implements CoffeeMachine {
 	}
 
 	public void insertCoin(Coin coin) throws CoffeeMachineException {
-		
-		
-		try{
+
+		try {
 			totalCoin += coin.getValue();
 			factory.getDisplay().info(
 					"Total: US$ " + totalCoin / 100 + "." + totalCoin % 100);
 
-		} catch(NullPointerException e){
+		} catch (NullPointerException e) {
 			throw new CoffeeMachineException("erro");
 		}
-		
+
 	}
 
+	public void cancel() throws CoffeeMachineException {
+
+		if (this.totalCoin == 0) {
+			throw new CoffeeMachineException("Nenhuma moeda inserida");
+		}
+		{
+
+		}
+
+	}
 }
