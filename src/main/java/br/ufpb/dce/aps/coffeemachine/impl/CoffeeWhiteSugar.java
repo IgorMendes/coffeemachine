1bf8be3c7305974315e0f84920c0f252798da9f7
unknown <igor.souza@dce.ufpb.br>
2014-08-19 10:04:28 -0300

    Task 1.26 finished for COMPOR

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/CoffeeWhiteSugar.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/CoffeeWhiteSugar.java
@@ -4,7 +4,7 @@ import net.compor.frameworks.jcf.api.Service;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 import br.ufpb.dce.aps.coffeemachine.Messages;
 
-public class CoffeeWhiteSugar extends CoffeeBlack {
+public class CoffeeWhiteSugar extends CoffeeWhite {
 
 	public CoffeeWhiteSugar(ComponentsFactory factory) {
 		super(factory);
@@ -12,32 +12,17 @@ public class CoffeeWhiteSugar extends CoffeeBlack {
 
 	@Service
 	public boolean verifyWhiteSugarDrink() {
-		if (!this.factory.getCupDispenser().contains(1)) {
-			this.factory.getDisplay().warn(Messages.OUT_OF_CUP);
+		if(!verifyWhiteDrink()){
 			return false;
 		}
-		if (!this.factory.getWaterDispenser().contains(80)) {
-			this.factory.getDisplay().warn(Messages.OUT_OF_WATER);
-			return false;
-		}
-		if (!this.factory.getCoffeePowderDispenser().contains(15)) {
-			this.factory.getDisplay().warn(Messages.OUT_OF_COFFEE_POWDER);
-			return false;
-		}
-		if (!factory.getCreamerDispenser().contains(20)) {
-			this.factory.getDisplay().warn(Messages.OUT_OF_CREAMER);
-			return false;
-		}
-		factory.getSugarDispenser().contains(100);
+		factory.getSugarDispenser().contains(5);
 		return true;
 
 	}
 
 	@Service
 	public void releaseWhiteSugarDrink() {
-		this.factory.getCoffeePowderDispenser().release(15);
-		this.factory.getWaterDispenser().release(80);
-		this.factory.getCreamerDispenser().release(20);;
-		factory.getSugarDispenser().release(100);
+		releaseWhiteDrink();
+		this.factory.getSugarDispenser().release(5);
 	}
 }
